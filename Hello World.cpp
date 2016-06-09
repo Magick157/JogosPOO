493c8c17dbc56dc35c1e70cd542ea9cb456327ed
Magick157 <josemarcelino987@gmail.com>
2016-06-09 10:26:57 -0300

    Atualização
    Meus Códigos Comentados

---
+++ Hello World.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //Incluindo a biblioteca
+using namespace std;
+
+
+int main(int argc, char* args[]) //função do meu programa
+{
+	cout << "Hello Word" << endl; //mostrando na tela a seguinte frase "Hello World"
+	return 0; //para o programa não voltar
+}
